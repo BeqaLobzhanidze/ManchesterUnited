b83aeb792101f86f5d335b0239c2eb83b4fc5a9c
BeqaLobzhanidze <65075819+BeqaLobzhanidze@users.noreply.github.com>
2020-11-21 16:59:35 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import FirstPage from "./Components/firstPage"
 import Players from "./Components/players"
 import Footer from "./Components/footer.js"
 import Header from './Components/header'
+import './Components/App.css'
 
 function App() {
   return (
